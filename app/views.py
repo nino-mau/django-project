f51ed5c39773ba7a2f31f7887ec859da0ec16ffc
nino <nino.mauuary@gmail.com>
2025-10-15 16:47:30 +0200

    [feat] create base layouts

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Bonjour")
+    context = {}
+    context["name"] = "toto"
+    return render(request, "index.html", context)

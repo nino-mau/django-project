27a05762ecf751db0ef37ecdd07ae92960e7a2f7
nino <nino.mauuary@gmail.com>
2025-10-16 11:26:55 +0200

    [build] remove bootstrap5 from project

--- app/settings.py
+++ app/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django_bootstrap5",
     "debug_toolbar",
     "django_extensions",
 ]
